a3551af280091375c195d477cb765ef171c2a88f
todd-trimmer <todd.trimmer@engineering11.com>
2026-05-19 10:02:43 -0500

    feat: new stuff

--- src/action.ts
+++ src/action.ts
@@ -1,19 +1,27 @@
 import * as core from '@actions/core'
+import { parse as parseDotenv } from 'dotenv'
+import { readFile } from 'fs/promises'
 import { generateNpmrc } from './core.js'
 
 async function run(): Promise<void> {
   try {
-    const template = core.getInput('template') || '.npmrc.tmpl'
+    const templates = core.getMultilineInput('templates')
     const output = core.getInput('output') || '.npmrc'
-    const token = core.getInput('token', { required: true })
+    const envFile = core.getInput('env-file')
+    const envLines = core.getMultilineInput('env')
 
-    core.setSecret(token)
-    process.env.GHPR_TOKEN = token
+    let env: Record<string, string> | undefined
+    if (envFile || envLines.length > 0) {
+      const fromFile = envFile ? parseDotenv(await readFile(envFile, 'utf8')) : {}
+      const fromInput = envLines.length > 0 ? parseDotenv(envLines.join('\n')) : {}
+      env = { ...fromFile, ...fromInput }
+    }
 
     await generateNpmrc({
-      templatePath: template,
+      templatePath: templates.length > 0 ? templates : '.npmrc.tmpl',
       outputPath: output,
       onMissing: 'empty',
+      env,
     })
   } catch (err) {
     core.setFailed((err as Error).message)
